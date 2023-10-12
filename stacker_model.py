44fa72deb2fa43e8313452314725e5b9d53a6c93
Collin <c22guo@uwaterloo.ca>
2023-10-12 13:34:38 -0400

    oop

--- stacker_model.py
+++ stacker_model.py
@@ -14,7 +14,7 @@ class StackerModel:
         self.prices3 = model3.predicted_prices
         self.labels = model1.labels
         self.real_prices = model1.raw_data["Close"].values[60:]
-        self.scaler = MinMaxScaler(feature_range=(0,1))
+        self.scaler = model1.scaler
         self.model = self.determine_weights()
         self.tmr_price = self.weighted_price(model1.tmr_price,model2.tmr_price,model3.tmr_price)
         
@@ -30,7 +30,6 @@ class StackerModel:
         return model
 
     def weighted_price(self, p1,p2,p3):
-        self.scaler = MinMaxScaler(feature_range=(0,1))
         predicted_price = self.model.predict(np.concatenate((np.array(p1),np.array(p2),np.array(p3)),axis=1))
         predicted_price = predicted_price.reshape(-1,1)
         predicted_price = self.scaler.inverse_transform(predicted_price)
@@ -40,8 +39,9 @@ class StackerModel:
         plt.plot(self.real_prices, color = "black")
         plt.plot(self.prices1, color = "green")
         plt.plot(self.prices2, color="orange")
-        plt.plot(self.prices3, color ="red")
+        plt.plot(self.prices3, color = "blue")
+        plt.plot(self.weighted_price(self.prices1,self.prices2, self.prices3), color= "red")
         plt.show()
-        # plt.plot(self.weighted_price(self.prices1,self.prices2, self.prices3))
+       
            
     
